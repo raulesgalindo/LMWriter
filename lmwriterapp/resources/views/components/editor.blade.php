ebb5370085d2a3d15fa11435065f3d498d65e19a
Raul Galindo <rgalindo@localhost.localdomain>
2018-05-16 20:55:00 -0400

    colocando iconos

--- lmwriterapp/resources/views/components/editor.blade.php
+++ lmwriterapp/resources/views/components/editor.blade.php
@@ -1,6 +1,8 @@
 
-<link rel="stylesheet" href="{{ asset('css/bootstrap-markdown.min.css') }}"  />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-markdown/2.10.0/css/bootstrap-markdown.min.css"  />
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
 <textarea name="content" id="content" data-provide="markdown" rows="10">{{ $file->content }}</textarea>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="{{ asset('js/bootstrap-markdown.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-markdown/2.10.0/js/bootstrap-markdown.js"></script>
+
 
